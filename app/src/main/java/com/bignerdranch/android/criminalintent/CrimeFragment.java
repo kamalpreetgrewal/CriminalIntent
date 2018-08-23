4dbded8e331f3cd2927c38709becd455453606bc
Kamalpreet Grewal <grewalkamal005@gmail.com>
2018-08-23 16:01:51 -0400

    First commit Setup of main crime activity Inclusion fragment in the main activity

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -0,0 +1,40 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+/**
+ * Created by Kamal on 23-08-2018.
+ */
+
+public class CrimeFragment extends Fragment {
+    private Crime mCrime;
+    private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        mTitleField = (EditText) v.findViewById(R.id.crime_title);
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+        mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+
+        return v;
+    }
+}
