eb4f844165be3160c68fd3b42092309af9084dcc
Kamalpreet Grewal <grewalkamal005@gmail.com>
2018-10-29 15:22:26 -0400

    Get data from CrimeLab and put it in the list view

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -41,17 +42,30 @@ public class CrimeListFragment extends Fragment {
     }
 
     /**
-     * This class creates a view holder for each of the crime items in the list.
+     * This class creates a view holder for each of the crime items in the list. Get each of the
+     * views in the item and assign them values in the bind method which is called in the adapter.
      */
     private class CrimeHolder extends RecyclerView.ViewHolder {
+        private TextView mTitleTextView;
+        private TextView mDateTextView;
+        private Crime mCrime;
+
         public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.list_item_crime, parent, false));
+            mTitleTextView = (TextView) itemView.findViewById(R.id.crime_title);
+            mDateTextView = (TextView) itemView.findViewById(R.id.crime_date);
+        }
+
+        public void bind(Crime crime) {
+            mCrime = crime;
+            mTitleTextView.setText(crime.getTitle());
+            mDateTextView.setText(crime.getDate().toString());
         }
     }
 
     /**
-     * This class creates a connection between the view holder for the crime item and
-     * the data.
+     * This class creates a connection between the view holder for the crime item and the data.
+     * Create a new view and bind data into it after getting position of the item in recyclerview.
      */
     private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder> {
         private List<Crime> mCrimes;
@@ -68,7 +82,8 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onBindViewHolder(CrimeHolder holder, int position) {
-
+            Crime crime = mCrimes.get(position);
+            holder.bind(crime);
         }
 
         @Override
